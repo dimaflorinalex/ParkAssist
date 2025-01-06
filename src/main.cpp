83b69700696c0e014db2b2d04bf1044ab50c19f6
Florin-Alexandru Dima <87763734+dimaflorinalex@users.noreply.github.com>
2025-01-06 17:30:13 +0200

    Milestone Software

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
 
+// #define DEBUG
+
+// Aux
+const unsigned long serialBaudRate = 115200;
+
 // LCD
-const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+const uint8_t lcdRsPin = 12, lcdEnPin = 11, lcdD4Pin = 10, lcdD5Pin = 9, lcdD6Pin = 8, lcdD7Pin = 7;
+LiquidCrystal lcd(lcdRsPin, lcdEnPin, lcdD4Pin, lcdD5Pin, lcdD6Pin, lcdD7Pin);
+const uint8_t lcdColumns = 16, lcdRows = 2;
+const unsigned long lcdDebounceMilliseconds = 500;
+unsigned long lcdLastUpdatedMilliseconds = 0;
+const unsigned long lcdMaxSensorDistanceMillimeters = 800;
 
 // Sensors (HC-SR04 Ultrasonic Sensors)
 enum Sensor {
@@ -12,34 +21,43 @@ enum Sensor {
   BACK = 1
 };
 
-const int trigPins[2] = { 13, 11 };
-const int echoPins[2] = { 12, 10 };
-uint32_t pulseDurations[2] = { 0, 0 };
-uint32_t spaceMillimeters[2] = { 0, 0 };
+const uint8_t sensorTrigPins[2] = { 6, A1 };
+const uint8_t sensorEchoPins[2] = { 5, A0 };
+unsigned long sensorPulseDurations[2] = { 0, 0 };
+unsigned long sensorDistanceMillimeters[2] = { UINT32_MAX, UINT32_MAX };
 
 // Speaker
-const int speakerPin = 3;
+const uint8_t speakerPin = 13;
+const unsigned int speakerToneFrequency = 2450;
+const unsigned long speakerToneDurationMilliseconds = 50;
+unsigned long speakerLastToneMilliseconds = 0;
+bool speakerToneEnabled = true;
 
 // Button
-const int buttonPin = 2;
-const int buttonDebounceMilliseconds = 100;
+const uint8_t buttonPin = 2;
+const unsigned long buttonDebounceMilliseconds = 100;
 unsigned long buttonLastPressedMilliseconds = 0;
+bool shouldHandleButton = false;
 
-void checkSensor(Sensor sensor)
-{
-  // Clear PIN_TRIGGER
-  digitalWrite(trigPins[sensor], LOW);
-  delayMicroseconds(2);
-  // Set PIN_TRIGGER for 10 micro seconds then clear
-  digitalWrite(trigPins[sensor], HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPins[sensor], LOW);
-  // Reads PIN_ECHO in micro
-  pulseDurations[sensor] = pulseIn(echoPins[sensor], HIGH);
-  // Calculate distance in mm s = v*t conversion from m to mm and s to us
-  spaceMillimeters[sensor] = pulseDurations[sensor]*340/2/1000;
-}
+// LED
+const int ledPin = 3;
+const unsigned long ledDebounceMilliseconds = 500;
+unsigned long ledLastUpdatedMilliseconds = 0;
 
+// Alarm
+enum Alarm {
+  NONE = 0,
+  LEVEL1 = 1,
+  LEVEL2 = 2,
+  LEVEL3 = 3
+};
+
+Alarm alarm = Alarm::NONE;
+const unsigned long alarmSpeakerDebounceMilliseconds[4] = { UINT32_MAX, 450, 300, 200 };
+const unsigned long alarmDistanceMillimeters[4] = { UINT32_MAX, 400, 250, 100 };
+const unsigned long alarmLedValues[4] = { 0, 127, 127, 255 };
+
+// Handle button interrupt
 void handleButtonInterrupt() {
  	int buttonValue = digitalRead(buttonPin);
   
@@ -48,51 +66,166 @@ void handleButtonInterrupt() {
   }
   else {
     if (millis() - buttonLastPressedMilliseconds >= buttonDebounceMilliseconds) {
-      Serial.println("Button pressed");
+      shouldHandleButton = true;
     }
     
     buttonLastPressedMilliseconds = 0;
   }
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Debug");
+// Handle button
+void handleButton() {
+  shouldHandleButton = false;
   
-  lcd.begin(16, 2);
-  lcd.clear();
-  lcd.print("Hello, World!");
+  speakerToneEnabled = !speakerToneEnabled;
+
+  char buffer[128] = "";
+  sprintf(buffer, "%lu ms - Handle button - Speaker tone enabled: %d", millis(), speakerToneEnabled);
+  Serial.println(buffer);
+}
+
+// Read value from ultrasonic sensor
+void readSensor(Sensor sensor)
+{
+  digitalWrite(sensorTrigPins[sensor], LOW);
+
+  delayMicroseconds(2);
+
+  digitalWrite(sensorTrigPins[sensor], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorTrigPins[sensor], LOW);
+
+  sensorPulseDurations[sensor] = pulseIn(sensorEchoPins[sensor], HIGH);
+  sensorDistanceMillimeters[sensor] = sensorPulseDurations[sensor] * 340/2/1000; // distance = speed of sound * time ; conversion to millimeters
+
+  if (sensorDistanceMillimeters[sensor] == 0) {
+    sensorDistanceMillimeters[sensor] = UINT32_MAX;
+  }
+
+  #ifdef DEBUG
+  char buffer[128] = "";
+  sprintf(buffer, "%lu ms - Sensor %d - Distance: %d mm", millis(), sensor, sensorDistanceMillimeters[sensor]);
+  Serial.println(buffer);
+  #endif
+}
+
+// Read values from all ultrasonic sensors
+void readSensors() {
+  for (int i = Sensor::FRONT; i <= Sensor::BACK; i++) {
+    readSensor((Sensor)i);
+  }
+}
+
+void updateDistancesLCD() {
+  unsigned long currentMilliseconds = millis();
+
+  if (currentMilliseconds - lcdLastUpdatedMilliseconds >= lcdDebounceMilliseconds) {
+    lcdLastUpdatedMilliseconds = currentMilliseconds;
+
+    lcd.clear();
+    char buffer[lcdColumns] = "";
+
+    if (sensorDistanceMillimeters[Sensor::FRONT] <= lcdMaxSensorDistanceMillimeters) {
+      sprintf(buffer, "Front: %lu mm", sensorDistanceMillimeters[Sensor::FRONT]);
+      lcd.print(buffer);
+    }
+    else {
+      lcd.print("Front: INF mm");
+    }
+    
+    lcd.setCursor(0, 1);
+
+    if (sensorDistanceMillimeters[Sensor::BACK] <= lcdMaxSensorDistanceMillimeters) {
+      sprintf(buffer, "Back: %lu mm", sensorDistanceMillimeters[Sensor::BACK]);
+      lcd.print(buffer);
+    }
+    else {
+      lcd.print("Back: INF mm");
+    }
+  }
+}
+
+void interpretSensors() {
+  unsigned long minSensorDistanceMillimeters = UINT32_MAX;
+
+  for (int i = Sensor::FRONT; i <= Sensor::BACK; i++) {
+  	if (sensorDistanceMillimeters[i] < minSensorDistanceMillimeters) {
+      minSensorDistanceMillimeters = sensorDistanceMillimeters[i];
+    }
+  }
+
+  for (int i = Alarm::LEVEL3; i >= Alarm::NONE; i--) {
+    if (minSensorDistanceMillimeters <= alarmDistanceMillimeters[i]) {
+      alarm = (Alarm)i;
+      return;
+    }
+  }
+
+  alarm = Alarm::NONE;
+}
+
+// Handle alarm
+void handleAlarm() {
+  #ifdef DEBUG
+  char buffer[128] = "";
+  sprintf(buffer, "%lu ms - Handle alarm - Alarm: %d - Speaker tone enabled: %d", millis(), alarm, speakerToneEnabled);
+  Serial.println(buffer);
+  #endif
+
+  unsigned long currentMilliseconds = millis();
+
+  if (currentMilliseconds - ledLastUpdatedMilliseconds >= ledDebounceMilliseconds) {
+    ledLastUpdatedMilliseconds = currentMilliseconds;
+
+    analogWrite(ledPin, alarmLedValues[alarm]);
+  }
+
+  if (alarm != Alarm::NONE) {
+    if (speakerToneEnabled) {
+      if (currentMilliseconds - speakerLastToneMilliseconds >= alarmSpeakerDebounceMilliseconds[alarm]) {
+        speakerLastToneMilliseconds = currentMilliseconds;
+
+        tone(speakerPin, speakerToneFrequency, speakerToneDurationMilliseconds);
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(serialBaudRate);
+  
+  #ifdef DEBUG
+  char buffer[128] = "";
+  sprintf(buffer, "%lu ms - Setup", millis());
+  Serial.println(buffer);
+  #endif
+  
+  lcd.begin(lcdColumns, lcdRows);
+  lcd.print("Park Assist");
+  lcd.setCursor(0, 1);
+  lcd.print("Initializing...");
+  lcdLastUpdatedMilliseconds = millis();
   
   pinMode(buttonPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(buttonPin), handleButtonInterrupt, CHANGE);
   
-  // pinMode(speakerPin, OUTPUT);
-  // digitalWrite(speakerPin, LOW);
+  pinMode(speakerPin, OUTPUT);
   
+  pinMode(ledPin, OUTPUT);
+
   for (int i = Sensor::FRONT; i <= Sensor::BACK; i++) {
-  	pinMode(trigPins[i], OUTPUT);
-    pinMode(echoPins[i], INPUT);
+  	pinMode(sensorTrigPins[i], OUTPUT);
+    pinMode(sensorEchoPins[i], INPUT);
   }
 }
 
 void loop() {
-  lcd.setCursor(0, 1);
-  lcd.print(millis() / 1000);
-  
-  // //tone(speakerPin, 820, 300);
-  
-  // delay(1000);
-  
-  // noTone(speakerPin);
-  
-  checkSensor(Sensor::FRONT);
-  checkSensor(Sensor::BACK);
-  
-  for (int i = Sensor::FRONT; i <= Sensor::BACK; i++) {
-    char buffer[50] = "";
-    sprintf(buffer, "Sensor %d: %d mm", i, spaceMillimeters[i]);
-  	Serial.println(buffer);
+  if (shouldHandleButton) {
+    handleButton();
   }
-  
-  delay(2000);
+
+  readSensors();
+  updateDistancesLCD();
+  interpretSensors();
+  handleAlarm();
 }
